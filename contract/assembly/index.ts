c89b0d404e7960ce07c89b1d683f3b07f08ba266
Alexander Sakhaev <alsakhaev@gmail.com>
2021-04-14 12:31:39 +0300

    docs: added jsdoc comments to contract code

--- contract/assembly/index.ts
+++ contract/assembly/index.ts
@@ -2,32 +2,53 @@ import { PersistentUnorderedMap } from "near-sdk-core";
 import { Context } from 'near-sdk-core';
 
 // MODELS
+
 const externalByNear = new PersistentUnorderedMap<string, string[]>("a");
 const nearByExternal = new PersistentUnorderedMap<string, string[]>("b");
 
 // READ
 
+/**
+ * Retrieves an array of external accounts for given NEAR Account ID
+ * @param near NEAR Account ID
+ * @returns Array of external accounts
+ */
 export function getExternalAccounts(near: string): string[] {
   return (externalByNear.contains(near)) ? externalByNear.getSome(near) : [];
 }
 
+/**
+ * Retrieves an array of NEAR Account IDs for given external account
+ * @param account External account
+ * @returns Array of NEAR Account IDs
+ */
 export function getNearAccounts(account: string): string[] {
   return (nearByExternal.contains(account)) ? nearByExternal.getSome(account) : [];
 }
 
 // WRITE
 
+/**
+ * Adds external account for NEAR Account ID
+ * @param account External account
+ */
 export function addExternalAccount(account: string): void {
   _insert(externalByNear, Context.sender, account);
   _insert(nearByExternal, account, Context.sender);
 }
 
+/**
+ * Removes external account for NEAR Account ID
+ * @param account External account
+ */
 export function removeExternalAccount(account: string): void {
   _delete(externalByNear, Context.sender, account);
   _delete(nearByExternal, account, Context.sender);
 }
 
-
+/**
+ * Clears all storage of the contract (for development stage)
+ */
 export function clearAll(): void {
   externalByNear.clear();
   nearByExternal.clear();
@@ -35,6 +56,12 @@ export function clearAll(): void {
 
 // HELPERS
 
+/**
+ * Inserts value to array stored in a map, if value is not exist
+ * @param map Map where to insert the value
+ * @param key Key in a map, by which array is stored
+ * @param value Value to be added to array
+ */
 function _insert(map: PersistentUnorderedMap<string, string[]>, key: string, value: string): void {
   if (!map.contains(key)) map.set(key, [value]);
 
@@ -48,6 +75,12 @@ function _insert(map: PersistentUnorderedMap<string, string[]>, key: string, val
   map.set(key, arr);
 }
 
+/**
+ * Deletes value from array stored in a map, if value is exist
+ * @param map Map where to delete the value
+ * @param key Key in a map, by which array is stored
+ * @param value Value to be removed from array
+ */
 function _delete(map: PersistentUnorderedMap<string, string[]>, key: string, value: string): void {
   if (!map.contains(key)) return;
 
